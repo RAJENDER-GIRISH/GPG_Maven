9d2f21c2f091e479a8eea97ef5538cda90c9b4d1
rajender.girish@gmail.com <rajender.girish@gmail.com>
2022-08-08 11:45:33 +0530

    first maven selenium test project

---
+++ src/main/java/GFG_MavenGFG_MAven/App.java
@@ -0,0 +1,13 @@
+package GFG_MavenGFG_MAven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
